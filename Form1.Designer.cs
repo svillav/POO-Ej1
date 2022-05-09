34220bc81e5af3ca10a736d6d970ea7347cf4cbb
Martin Ballester <martinballester1@gmail.com>
2022-05-09 17:07:36 -0300

    metodo add actualizado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,109 +32,142 @@ namespace POO_Ejercicio1
             this.nombre = new System.Windows.Forms.Label();
             this.apellido = new System.Windows.Forms.Label();
             this.edad = new System.Windows.Forms.Label();
-            this.textNombre = new System.Windows.Forms.TextBox();
-            this.textApellido = new System.Windows.Forms.TextBox();
-            this.textEdad = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textAge = new System.Windows.Forms.TextBox();
             this.botonAgregar = new System.Windows.Forms.Button();
             this.botonBorrar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.dgvName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
             this.SuspendLayout();
             // 
             // nombre
             // 
             this.nombre.AutoSize = true;
-            this.nombre.Location = new System.Drawing.Point(50, 48);
+            this.nombre.Location = new System.Drawing.Point(33, 31);
+            this.nombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nombre.Name = "nombre";
-            this.nombre.Size = new System.Drawing.Size(65, 20);
+            this.nombre.Size = new System.Drawing.Size(44, 13);
             this.nombre.TabIndex = 0;
             this.nombre.Text = "Nombre";
             // 
             // apellido
             // 
             this.apellido.AutoSize = true;
-            this.apellido.Location = new System.Drawing.Point(50, 112);
+            this.apellido.Location = new System.Drawing.Point(33, 73);
+            this.apellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.apellido.Name = "apellido";
-            this.apellido.Size = new System.Drawing.Size(65, 20);
+            this.apellido.Size = new System.Drawing.Size(44, 13);
             this.apellido.TabIndex = 1;
             this.apellido.Text = "Apellido";
             // 
             // edad
             // 
             this.edad.AutoSize = true;
-            this.edad.Location = new System.Drawing.Point(63, 179);
+            this.edad.Location = new System.Drawing.Point(42, 116);
+            this.edad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.edad.Name = "edad";
-            this.edad.Size = new System.Drawing.Size(47, 20);
+            this.edad.Size = new System.Drawing.Size(32, 13);
             this.edad.TabIndex = 2;
             this.edad.Text = "Edad";
             // 
-            // textNombre
+            // textName
             // 
-            this.textNombre.Location = new System.Drawing.Point(147, 45);
-            this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(224, 26);
-            this.textNombre.TabIndex = 3;
+            this.textName.Location = new System.Drawing.Point(98, 29);
+            this.textName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(151, 20);
+            this.textName.TabIndex = 3;
             // 
-            // textApellido
+            // textLastName
             // 
-            this.textApellido.Location = new System.Drawing.Point(147, 109);
-            this.textApellido.Name = "textApellido";
-            this.textApellido.Size = new System.Drawing.Size(224, 26);
-            this.textApellido.TabIndex = 4;
+            this.textLastName.Location = new System.Drawing.Point(98, 71);
+            this.textLastName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(151, 20);
+            this.textLastName.TabIndex = 4;
             // 
-            // textEdad
+            // textAge
             // 
-            this.textEdad.Location = new System.Drawing.Point(147, 176);
-            this.textEdad.Name = "textEdad";
-            this.textEdad.Size = new System.Drawing.Size(224, 26);
-            this.textEdad.TabIndex = 5;
+            this.textAge.Location = new System.Drawing.Point(98, 114);
+            this.textAge.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textAge.Name = "textAge";
+            this.textAge.Size = new System.Drawing.Size(151, 20);
+            this.textAge.TabIndex = 5;
             // 
             // botonAgregar
             // 
-            this.botonAgregar.Location = new System.Drawing.Point(67, 271);
+            this.botonAgregar.Location = new System.Drawing.Point(45, 176);
+            this.botonAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.botonAgregar.Name = "botonAgregar";
-            this.botonAgregar.Size = new System.Drawing.Size(141, 58);
+            this.botonAgregar.Size = new System.Drawing.Size(94, 38);
             this.botonAgregar.TabIndex = 6;
             this.botonAgregar.Text = "Agregar";
             this.botonAgregar.UseVisualStyleBackColor = true;
-            this.botonAgregar.Click += new System.EventHandler(this.button1_Click);
+            this.botonAgregar.Click += new System.EventHandler(this.add_Click);
             // 
             // botonBorrar
             // 
-            this.botonBorrar.Location = new System.Drawing.Point(230, 271);
+            this.botonBorrar.Location = new System.Drawing.Point(153, 176);
+            this.botonBorrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.botonBorrar.Name = "botonBorrar";
-            this.botonBorrar.Size = new System.Drawing.Size(141, 58);
+            this.botonBorrar.Size = new System.Drawing.Size(94, 38);
             this.botonBorrar.TabIndex = 7;
             this.botonBorrar.Text = "Borrar";
             this.botonBorrar.UseVisualStyleBackColor = true;
+            this.botonBorrar.Click += new System.EventHandler(this.delete_Click);
             // 
-            // dataGridView1
+            // dataGridViewUsers
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(429, 45);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(464, 284);
-            this.dataGridView1.TabIndex = 8;
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvName,
+            this.dgvLastName,
+            this.dgvAge});
+            this.dataGridViewUsers.Location = new System.Drawing.Point(286, 29);
+            this.dataGridViewUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.RowHeadersWidth = 62;
+            this.dataGridViewUsers.RowTemplate.Height = 28;
+            this.dataGridViewUsers.Size = new System.Drawing.Size(375, 185);
+            this.dataGridViewUsers.TabIndex = 8;
+            // 
+            // dgvName
+            // 
+            this.dgvName.HeaderText = "Nombre";
+            this.dgvName.Name = "dgvName";
+            // 
+            // dgvLastName
+            // 
+            this.dgvLastName.HeaderText = "Apellido";
+            this.dgvLastName.Name = "dgvLastName";
+            // 
+            // dgvAge
+            // 
+            this.dgvAge.HeaderText = "Edad";
+            this.dgvAge.Name = "dgvAge";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(973, 388);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(672, 252);
+            this.Controls.Add(this.dataGridViewUsers);
             this.Controls.Add(this.botonBorrar);
             this.Controls.Add(this.botonAgregar);
-            this.Controls.Add(this.textEdad);
-            this.Controls.Add(this.textApellido);
-            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textAge);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textName);
             this.Controls.Add(this.edad);
             this.Controls.Add(this.apellido);
             this.Controls.Add(this.nombre);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,12 +178,15 @@ namespace POO_Ejercicio1
         private System.Windows.Forms.Label nombre;
         private System.Windows.Forms.Label apellido;
         private System.Windows.Forms.Label edad;
-        private System.Windows.Forms.TextBox textNombre;
-        private System.Windows.Forms.TextBox textApellido;
-        private System.Windows.Forms.TextBox textEdad;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textAge;
         private System.Windows.Forms.Button botonAgregar;
         private System.Windows.Forms.Button botonBorrar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvAge;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,23 @@ namespace POO_Ejercicio1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void add_Click(object sender, EventArgs e)
+        {
+            //n = indice del registro
+            int n = dataGridViewUsers.Rows.Add();
+
+            //agregar nombre, apellido y edad al gridView
+            dataGridViewUsers.Rows[n].Cells[0].Value = textName.Text;
+            dataGridViewUsers.Rows[n].Cells[1].Value = textLastName.Text;
+            dataGridViewUsers.Rows[n].Cells[2].Value = textAge.Text;
+
+            textName.Text = "";
+            textLastName.Text = "";
+            textAge.Text = "";
+    
+        }
+
+        private void delete_Click(object sender, EventArgs e)
         {
 
         }
